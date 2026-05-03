3fbdb8b0c7eb9dd781e98c1309c13224577a956f
Mhai <mpavila2031@crc.pshs.edu.ph>
2026-05-03 21:39:14 +0800

    Update Acadtrack.py

--- Acadtrack.py
+++ Acadtrack.py
@@ -1,3 +1,21 @@
 """
 project Title: Acadtrack - A Student Academic Deadline Tracker
+group Members:
+- Mhai Princess Kayeshea Avila
+- Liam Isaac Sudario
+- Alfonso Ace Magdaluyo
+
+Brief description: 
+AcadTrack is a simple Python Program that helps students track their
+academic tasks such as assignments, quizzes, projects, and exams.
+It allows the user to add tasks, view tasks, mark tasks as completed, 
+and delete tasks.
 """
+
+from datetime import datetime
+
+task_list = []
+
+def ShowMenu():
+  print("\n===== ACADTRACK MENU =====")
+  print("1. Add task")
